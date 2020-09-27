0241541267826df6ef921fa850ba73f707477edf
ghdry2563 <ghdry2563@gmail.com>
2020-09-27 18:22:16 +0900

    start project

--- rdbms/1.CREATE_TABLES.sql
+++ rdbms/1.CREATE_TABLES.sql
@@ -1,15 +1,15 @@
 -- 질문 테이블
 create table QUESTION(
-	ID INT not null AUTO_INCREMENT PRIMARY KEY,
-	TITLE TEXT not null,
-	QUESION TEXT not null
+	Q_ID INT not null autoincrement PRIMARY KEY,
+	TITLE string not null,
+	QUESION string not null
 	);
 
 -- 선택지 테이블
 create table CHOICES(
 	Q_ID INT not null,
 	C_NUMBER INT not null,
-	TEXT TEXT not null,
+	TEXT string not null,
     POINT INT,
 	FOREIGN KEY(Q_ID) REFERENCES QUESTION(ID),
 	PRIMARY KEY(Q_ID, C_NUMBER)
@@ -17,14 +17,15 @@ create table CHOICES(
 
 -- 
 create table SURV_RESULT(
-    E_MAIL TEXT NOT NULL,
+    E_MAIL string NOT NULL,
     Q_ID INT NOT NULL,
-    C_NUMBER INT NOT null
+    C_NUMBER INT NOT null,
     PRIMARY KEY(E_MAIL, Q_ID)
 )
 
 create table SURV_USER(
-    ID INT NOT NULL,
-    NAME VARCHAR(32) NOT NULL
+    E_MAIL string NOT NULL,
+	AGE int not null,
+	FOREIGN KEY(Q_ID) REFERENCES QUESTION(ID),
     PRIMARY KEY(ID)
 )
