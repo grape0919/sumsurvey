f9e732db50722488efd9bae8205ed3bf7b149bce
ghdry2563 <ghdry2563@gmail.com>
2020-10-01 01:51:18 +0900

    중간 커밋

--- sumsurvey.py
+++ sumsurvey.py
@@ -19,11 +19,13 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 def connect_db():
-    print(app.config)
-    return sqlite3.connect(app.config['DATABASE'])
+    return sqlite3.connect(DATABASE)
 
 def init_db():
     with closing(connect_db()) as db:
+        with app.open_resource('rdbms\\0.DROP_TABLES.sql') as f:
+            db.cursor().executescript(f.read().decode('utf-8'))
+        db.commit()
         with app.open_resource('rdbms\\1.CREATE_TABLES.sql') as f:
             db.cursor().executescript(f.read().decode('utf-8'))
         db.commit()
@@ -36,23 +38,21 @@ def before_request():
 def teardown_request(exception):
     g.db.close()
 
-@app.route('/')
-def show_entries():
-    cur = g.db.execute('select * from question')
-    print("!@#!@# cur : ", cur.fetchall())
-    entries = []
-    entries.append(dict(title="취미_종류", question="나의 취미는?"))
-    entries.append(dict(title="무인도_집", question="무인도에서 일주일동안 지낼 집을 지어야 한다. 내 집은..."))
-    entries.append(dict(title="무인도_화장실", question="그렇다면 무인도에서 지낼 화장실은...?"))
-    entries.append(dict(title="여행_식사", question="나는 여행지에서 식사를 주로,,,"))
-    entries.append(dict(title="개인_차량", question="나는 차량이 있다/없다."))
-    entries.append(dict(title="", question=""))
-    entries.append(dict(title="", question=""))
-    entries.append(dict(title="", question=""))
-    entries.append(dict(title="", question=""))
-    entries.append(dict(title="", question=""))
+@app.route('/survey/<id>')
+def show_entries(id=None):
+    cur = g.db.cursor().execute('SELECT TITLE, QUESTION FROM QUESTION')
+    g.db.commit()# cur = g.db.cursor().execute('SELECT * FROM QUESTION;')
+    entries = [dict(title=row[0], question=row[1]) for row in cur.fetchall()]
     print("!@#!@# entries : ", entries)
     return render_template('show_entries.html', entries=entries)
+
+@app.route('/')
+def start():
+    return render_template('start_surv.html', image_file="resources/img/start.jpg")
+
+@app.route('/analz')
+def analz():
+    return render_template('start_surv.html')
 
 if __name__ == '__main__':
     app.run()
